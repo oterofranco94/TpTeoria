decad2903b0d5ebc34db1aea19428cf8ccbb36fe
Franco Otero <franco.otero94@gmail.com>
2020-05-12 20:19:24 -0300

    clean

--- src/tpteoria/TPTeoria.java
+++ src/tpteoria/TPTeoria.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tpteoria;
 
-/**
- *
- * @author francootero
- */
-public class TPTeoria {
 
-    /**
-     * @param args the command line arguments
-     */
+public class TPTeoria {
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
