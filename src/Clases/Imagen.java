d0ca18ba2c7a471df2f53d248b833dda5ae9bfbf
Franco Otero <franco.otero94@gmail.com>
2020-05-25 18:45:25 -0300

    Creacion UI

---
+++ src/Clases/Imagen.java
@@ -0,0 +1,19 @@
+package Clases;
+
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+
+public class Imagen {
+    private static int ancho = 1310;
+    private static int alto = 1700;
+    
+    public Imagen(){}
+    
+    
+        //InputStream input = new FileInputStream("/Users/francootero/Downloads/ImagenesWill/Will(Original).bmp");
+        //ImageInputStream imageInput = ImageIO.createImageInputStream(input);
+        //BufferedImage original = ImageIO.read(imageInput);
+}
--- src/tpteoria/TPTeoria.java
+++ src/tpteoria/TPTeoria.java
@@ -1,9 +1,23 @@
 package tpteoria;
 
+import UI.Menu;
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+
 
 public class TPTeoria {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Menu ui = new Menu();
+        ui.setVisible (true);
+        InputStream input = new FileInputStream("/Users/francootero/Downloads/ImagenesWill/Will(Original).bmp");
+        ImageInputStream imageInput = ImageIO.createImageInputStream(input);
+        BufferedImage original = ImageIO.read(imageInput);
+        System.out.println(original.getWidth() +"and"+ original.getHeight());
     }
     
 }
