373bb8d3cf56d4a2ff0cb3c5e6f591f5dd48b6ec
Franco Otero <franco.otero94@gmail.com>
2020-05-26 21:19:34 -0300

    Image Class

--- src/Clases/Imagen.java
+++ src/Clases/Imagen.java
@@ -1,19 +1,40 @@
 package Clases;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Vector;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
 
 public class Imagen {
-    private static int ancho = 1310;
-    private static int alto = 1700;
+    private static int width = 1310;
+    private static int height = 1700;
     
-    public Imagen(){}
+    public Imagen(String path) {
+    try 
+    {
+      BufferedImage image = ImageIO.read(this.getClass().getResource(path));
+      getSecuencia(image);
+    } 
+    catch (IOException e) 
+    {
+      System.err.println(e.getMessage());
+    }
+  }
     
-    
-        //InputStream input = new FileInputStream("/Users/francootero/Downloads/ImagenesWill/Will(Original).bmp");
-        //ImageInputStream imageInput = ImageIO.createImageInputStream(input);
-        //BufferedImage original = ImageIO.read(imageInput);
+    public Vector<Integer> getSecuencia(BufferedImage image) {
+        Vector<Integer> secuencia = new Vector<Integer>();
+	  for (int x = 0; x < width; x++) {
+		  for (int y = 0; y < height; y++) {
+			  int rgb = image.getRGB(x, y);
+			  Color color = new Color(rgb, true);
+			  int codigo= color.getRed();
+			  secuencia.add(codigo);
+		  }
+	  }
+          return secuencia;
+  }
 }
--- src/tpteoria/TPTeoria.java
+++ src/tpteoria/TPTeoria.java
@@ -1,11 +1,13 @@
 package tpteoria;
 
+import Clases.Imagen;
 import UI.Menu;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Vector;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
 
@@ -13,6 +15,12 @@ import javax.imageio.stream.ImageInputStream;
 public class TPTeoria {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Menu ui = new Menu();
-        ui.setVisible (true);
+        //ui.setVisible (true);
+        Imagen willOriginal = new Imagen("../Imagenes/Will(Original).bmp");
+        Imagen will1 = new Imagen("../Imagenes/Will_1.bmp");
+        //Imagen will2 = new Imagen("../Imagenes/Will_2.bmp");
+        //Imagen will3 = new Imagen("../Imagenes/Will_3.bmp");
+        //Imagen will4 = new Imagen("../Imagenes/Will_4.bmp");
+        //Imagen will5 = new Imagen("../Imagenes/Will_5.bmp");
     }
 }
