bb5713ccdd4a1a2fe491b6c9ac9fcd3827a5e6ef
Federico Simaro <fede.simaro@gmail.com>
2020-05-27 19:13:43 -0300

    Metodos media, varianza, covarianza y correlacion en imagen

--- src/Clases/Imagen.java
+++ src/Clases/Imagen.java
@@ -9,15 +9,21 @@ import java.util.Vector;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
 
+
 public class Imagen {
-    private static int width = 1310;
-    private static int height = 1700;
+    private static int colores = 256;
+    private static int width=1310;
+    private static int height=1700;
+    private static BufferedImage image;
     
     public Imagen(String path) {
     try 
     {
-      BufferedImage image = ImageIO.read(this.getClass().getResource(path));
-      getSecuencia(image);
+      image = ImageIO.read(this.getClass().getResource(path));
+      this.width = image.getWidth();
+      this.height = image.getHeight();
+      
+      //getSecuencia(image);
     } 
     catch (IOException e) 
     {
@@ -25,7 +31,16 @@ public class Imagen {
     }
   }
     
-    public Vector<Integer> getSecuencia(BufferedImage image) {
+    
+    public int getWidth(){
+        return width;
+    }
+    
+    public int getHeight(){
+        return height;
+    }
+    
+    public Vector<Integer> getSecuencia() {
         Vector<Integer> secuencia = new Vector<Integer>();
 	  for (int x = 0; x < width; x++) {
 		  for (int y = 0; y < height; y++) {
@@ -37,4 +52,48 @@ public class Imagen {
 	  }
           return secuencia;
   }
+    
+    public double getMedia (){
+        Vector<Integer> aux = getSecuencia();
+        double suma = 0;
+        for (int i=0;i<aux.size();i++){
+            suma+=aux.elementAt(i);}
+           
+        
+        return suma / aux.size();
+    }
+    
+    public double getDesvioEstandar(){
+        Vector<Integer> aux = getSecuencia();
+        double media = getMedia();
+        double varianza = 0;
+        for (int i=0;i<aux.size();i++)
+            varianza += Math.pow((aux.elementAt(i)-media), 2);
+        
+        return Math.sqrt(varianza/aux.size());
+    }
+    
+    public double getCovarianza (Imagen imagenB){
+        
+        Vector<Integer> secuenciaA = getSecuencia();
+        Vector<Integer> secuenciaB = imagenB.getSecuencia();
+        double mediaA = getMedia();
+        double mediaB = imagenB.getMedia();
+        double covarianza = 0;
+        for (int i=0;i<secuenciaA.size();i++){
+            covarianza+= ( (secuenciaA.elementAt(i)-mediaA)*(secuenciaB.elementAt(i)-mediaB))/secuenciaA.size();
+        }
+        
+        return covarianza;
+    }
+    
+    public double getFactorCorrelacion(Imagen imagenB){
+        
+        double factor = (getCovarianza(imagenB)/(getDesvioEstandar()*imagenB.getDesvioEstandar()));
+        
+        return factor;
+    }
+    
+    
+ 
 }
--- src/tpteoria/TPTeoria.java
+++ src/tpteoria/TPTeoria.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
 import java.util.Vector;
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
@@ -14,13 +15,32 @@ import javax.imageio.stream.ImageInputStream;
 
 public class TPTeoria {
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        Menu ui = new Menu();
+        //Menu ui = new Menu();
         //ui.setVisible (true);
         Imagen willOriginal = new Imagen("../Imagenes/Will(Original).bmp");
         Imagen will1 = new Imagen("../Imagenes/Will_1.bmp");
-        //Imagen will2 = new Imagen("../Imagenes/Will_2.bmp");
-        //Imagen will3 = new Imagen("../Imagenes/Will_3.bmp");
-        //Imagen will4 = new Imagen("../Imagenes/Will_4.bmp");
-        //Imagen will5 = new Imagen("../Imagenes/Will_5.bmp");
+        Imagen will2 = new Imagen("../Imagenes/Will_2.bmp");
+        Imagen will3 = new Imagen("../Imagenes/Will_3.bmp");
+        Imagen will4 = new Imagen("../Imagenes/Will_4.bmp");
+        Imagen will5 = new Imagen("../Imagenes/Will_5.bmp");
+        
+        System.out.print("El factor de correlacion entre la foto original y will1 es : "+willOriginal.getFactorCorrelacion(will1)+"\n");
+        System.out.print("El factor de correlacion entre la foto original y will2 es : "+willOriginal.getFactorCorrelacion(will2)+"\n");
+        System.out.print("El factor de correlacion entre la foto original y will3 es : "+willOriginal.getFactorCorrelacion(will3)+"\n");
+        System.out.print("El factor de correlacion entre la foto original y will4 es : "+willOriginal.getFactorCorrelacion(will4)+"\n");
+        System.out.print("El factor de correlacion entre la foto original y will5 es : "+willOriginal.getFactorCorrelacion(will5)+"\n");
+
+        Imagen [] parecidos = {will1,will2,will3,will4,will5};
+       // ejercicio1(willOriginal, parecidos);
     }
+    
+    public static void ejercicio1 (Imagen original, Imagen[] parecidos){
+        
+        
+        
+        
+    }
+    
+    
+    
 }
